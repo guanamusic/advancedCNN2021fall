2ad769f4128dd32847c9a2950510ab6198db6819
guana <guana@kaist.ac.kr>
2021-12-04 23:54:45 +0900

    fix superclass

--- model/layers.py
+++ model/layers.py
@@ -15,7 +15,8 @@ class Conv1dWithInitialization(BaseModule):
     def forward(self, x):
         return self.conv1d(x)
 
-class DepthwiseConv(nn.Module):
+
+class DepthwiseConv(BaseModule):
     def __init__(self, nin, kernels_per_layer, kernel_size):
         super(DepthwiseConv, self).__init__()
         self.depthwise = torch.nn.Conv2d(nin, nin*kernels_per_layer, kernel_size, padding=1, groups=nin)
